3b30996659348d362b4b6b35882a1eb3d376d930
John Simpson <simpson@adobe.com>
2024-10-03 14:06:17 -0700

    setting up project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 TODO JLS
+
+* Login
+* Page for members. Create / Edit users
+* Page for creating / editing labels
+* Page for showing members of labels
+* Page for Calandar
+* Page for seeing / setting availability
+* Page for scheduling days. (Select a label to view memebers. Also see members already scheduled for that day)
+* Page for creating task
+* Page for viewing tasks
--- app/page.tsx
+++ app/page.tsx
@@ -10,43 +10,20 @@ import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+  // function listTodos() {
+  //   client.models.Todo.observeQuery().subscribe({
+  //     next: (data) => setTodos([...data.items]),
+  //   });
+  // }
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+      <h1>CTV Stuff</h1>
     </main>
   );
 }
