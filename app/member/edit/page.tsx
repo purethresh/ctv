4df899c8cb927f3ef864674fd371bca8ab432d69
John Simpson <simpson@adobe.com>
2024-10-09 07:47:55 -0700

    cleanup

--- app/member/edit/page.tsx
+++ app/member/edit/page.tsx
@@ -1,4 +1,12 @@
 
+// https://docs.amplify.aws/react/build-a-backend/data/connect-to-existing-data-sources/connect-postgres-mysql-database/
+
+// TODO JLS
+// * Figure out how to create a sandbox?
+// * Connect to DB
+// * Should make schema
+// * See if we can get / insert / update data
+
 export default function EditMemberPage() {
   return (
     <div>edit member</div>
