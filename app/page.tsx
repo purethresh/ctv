680561bcef2d7b550932f57cea47fb7bca51f1d4
John Simpson <simpson@adobe.com>
2024-11-18 12:05:11 -0800

    start for building home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,15 @@ Amplify.configure(outputs);
 
 export default function App() {
 
+  // TODO JLS
+  // * Get cookie to see if user is logged in our guest
+  // * If not logged in / guest, redirect to login page
+  // * If Logged in, show main page (Callander, Labels for each day)
+  // * Create User / Church selector
+  // * Create App Bar with name of church, and logout / switch user button
+  // * Create Calendar component
+  // * Create component to show labels for selected day
+
   return (
     <main>
       Main Page
