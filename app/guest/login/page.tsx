3890dcbb6f734a915178f576fbe02a94ef848bb3
John Simpson <simpson@adobe.com>
2024-11-18 16:33:40 -0800

    update comment

--- app/guest/login/page.tsx
+++ app/guest/login/page.tsx
@@ -1,10 +1,13 @@
-// TODO JLS
+// TODO JLS HERE !!!
+//
+// For DB, need church table
+// For DB, need member table
 //
 // Show Login
 // OR
 // signin as guest
 //
-// After login or guest, set cookies and redirect back  
+// After login or guest, set cookies and redirect back
 
 
 export default function LoginPage() {
