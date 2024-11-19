851c8dd651263714b735a28e5191e0eb5040fce0
John Simpson <simpson@adobe.com>
2024-11-18 16:31:53 -0800

    starting login page

--- app/guest/login/page.tsx
+++ app/guest/login/page.tsx
@@ -1,12 +1,13 @@
-  // TODO JLS
-  // * Create User / Church selector
-  // * Or allow Login
-  // * After Login, or guest, go Back to previous page
+// TODO JLS
+//
+// Show Login
+// OR
+// signin as guest
+//
+// After login or guest, set cookies and redirect back  
+
 
 export default function LoginPage() {
-    // Option to login, or continue as guest
-    // If Continue as guest, set cookie to guest and redirect to main page
-    // After Login, set cookie to member and redirect to main page
   return (
     <div>Login page</div>
   );
