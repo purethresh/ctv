33771899ffe1684a560187a1baf8419de4f97d99
John Simpson <simpson@adobe.com>
2024-11-21 10:25:06 -0800

    comment

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,7 @@ import UserInfo from "./lib/UserInfo";
 Amplify.configure(outputs);
 
 // TODO JLS HERE
-// Create a Lib for getting user Info
-// 
-// Then call that here at the page level
-// Pass the results into SNavbar, SChurchCalendar, and SLabelDay
-//
-// *
-// * Then pass them into the SCalendar component.
-// * Get called when selected date is changed
-// * Pass that into SLabelDay
-// * Implement SLabelDay
+// SServies should show a list of services for the selected day
 
 export default function App() {
   let [selectedDay, setSelectedDay] = useState<string>(getDefaultSunday());
