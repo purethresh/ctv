524c5bd7a660d3c77642dbd0cbc8471e26a5aa42
John Simpson <simpson@adobe.com>
2024-12-05 13:23:50 -0800

    starting to save phone numbers

--- app/components/SMemberPhoneList.tsx
+++ app/components/SMemberPhoneList.tsx
@@ -1,11 +1,65 @@
 import { SMemberInfoProp } from "../props/SMemberInfoProp";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button, TextField, Checkbox } from "@mui/material";
 import { useEffect, useState } from "react";
-import { IPhoneInfo } from "../lib/IPhoneInfo";
+import { MemberPhoneInfo } from "../lib/MemberPhoneInfo";
+import { v4 } from 'uuid';
 
 export default function SMemberPhoneList(props:SMemberInfoProp) {
     let [memberId, setMemberId] = useState<string>('');
-    let [phoneList, setPhoneList] = useState<IPhoneInfo[]>([]);
+    let [phoneList, setPhoneList] = useState<MemberPhoneInfo[]>([]);
+    let [isDirty, setIsDirty] = useState<boolean>(false);
+    let [isEditing, setIsEditing] = useState<boolean>(props.isEditing ? true : false);
+
+    const onAddPhone = () => {
+        const pInfo = new MemberPhoneInfo({});
+        pInfo.phone_id = v4();
+        pInfo.member_id = memberId;
+
+        const lst = Array.from(phoneList);
+        lst.push(pInfo);
+
+        setPhoneList(lst);
+        setIsDirty(true);
+    }
+
+    const updatePhoneNumber = (pId:string, pNumber:string) =>{
+        // TODO JLS
+    }
+
+    const updateIsPrimary = (pId:string, isPrimary:boolean) => {
+        // TODO JLS
+    }
+    
+    const onSave = async() => {
+        // Ignore if we are not editing
+        if (props.needsSave === false) {
+            return;
+        }
+
+        if (!isDirty) {
+            // No need to save, so inform parent that save is done
+            if (props.onSaveComplete) {
+                props.onSaveComplete();
+            }
+            return;
+        }
+        else {
+            // Save goes here
+        }        
+    }
+
+    // TODO JLS
+    // Need to create a map of items that need to be created and another map for those that need updating
+    // Then need to handle save when needsSave is true
+    // Finially need to call onSaveComplete when saving is done
+
+    const updateIsEditing = () => {
+        const editing = props.isEditing ? true : false;
+
+        if (editing !== isEditing) {
+            setIsEditing(editing);
+        }
+    }
 
     const updateMemberInfo = async() => {
         
@@ -24,18 +78,33 @@ export default function SMemberPhoneList(props:SMemberInfoProp) {
         if (rs.length > 0) {
             setPhoneList(rs);
         }
+        else {
+            setPhoneList([]);
+        }
     }
 
     useEffect(() => {
         updateMemberInfo();
-    }, [props.memberId, props.isAdmin]); 
+    }, [props.memberId, props.isAdmin]);
+    
+    useEffect(() => {
+        updateIsEditing();
+    }, [props.isEditing]);
+
+    useEffect(() => {
+        onSave();
+    }, [props.needsSave]);      
 
     return (
         <>
             {phoneList.map((pInfo, index) => (
-                <Box><Typography>{pInfo.pNumber}</Typography></Box>
-            ))}        
+                <Box key={index}>
+                    <Typography style={{display:isEditing ? 'none' : 'block'}}>{pInfo.pNumber}</Typography>
+                    <TextField style={{display:isEditing ? 'block' : 'none'}} label="Phone Number" defaultValue={pInfo.pNumber} onChange={(event: React.ChangeEvent<HTMLInputElement>) => { updatePhoneNumber(pInfo.phone_id, event.target.value); }} />
+                    <Checkbox style={{display:isEditing ? 'block' : 'none'}} defaultChecked={pInfo.isPrimary === 'true'} onChange={(event: React.ChangeEvent<HTMLInputElement>) => { updateIsPrimary(pInfo.phone_id, event.target.checked); }} />
+                </Box>
+            ))}         
+            <Button onClick={onAddPhone} style={{display:isEditing ? 'block' : 'none'}}>Add Phone</Button>
         </>
     );
-
 }
--- app/lib/IPhoneInfo.ts
+++
@@ -1,6 +0,0 @@
-export interface IPhoneInfo {
-    phone_id?:string;
-    member_id?:string;
-    pNumber?:string;
-    isPrimary?:string;
-};
